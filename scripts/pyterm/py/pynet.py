4b688be5c9efdfd56eb631a1bae594034f8fcf53
SlashCreate <112573796+SlashCreate@users.noreply.github.com>
2023-02-06 15:46:37 -0500

    Update pynet.py

--- scripts/pyterm/py/pynet.py
+++ scripts/pyterm/py/pynet.py
@@ -1,1 +1,5 @@
-print('Currently In Development')
+import urllib.request  # the lib that handles the url stuff
+
+def getfile(url: str):
+  for line in urllib.request.urlopen(url):
+    print(line.decode('utf-8'))
