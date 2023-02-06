47ce639706433f1dcae80337503cc47d3664af24
SlashCreate <112573796+SlashCreate@users.noreply.github.com>
2023-02-06 15:57:43 -0500

    Update pyterm.py

--- scripts/pyterm/py/pyterm.py
+++ scripts/pyterm/py/pyterm.py
@@ -1,5 +1,26 @@
 # Python Terminal (PYTERM)
 from pynet import getfile
+import os
+
+filepath = 'version.CONFIG'
+with open(filepath) as fp:
+   line = fp.readline()
+   cnt = 1
+   while line:
+        arg = line.strip()
+        b = arg.split("''")
+       
+        if b[0] == '-CONFIG':
+            conf = True
+        elif b[0] == '.setting':
+            if b[1] == 'user':
+                user = b[2]
+            elif b[1] == 'ver':
+                ver == b[2]
+        else:
+            print('err')
+        line = fp.readline()
+        cnt += 1
 
 def windows()
     return platform.system() == "Windows"
@@ -8,7 +29,7 @@ def linux():
     return platform.system() == "Linux"
 
 while True:
-  a = input('pyterm > ').split(']]')
+  a = input(f'{user} > ').split(']]')
   try:
     if a[0] == '.out':
       print(a[1])
@@ -23,5 +44,7 @@ while True:
       print('''.out]]<text>                 Print <text> onto screen
 .help                        Help menu (This screen)
 .clr                         Clears screen (Only works for Windows and Lunix)''')
+    else:
+        pass
   except:
     print('ERROR')
